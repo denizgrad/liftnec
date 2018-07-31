1160714891c0601e8ab414e5d010e709493703da
Mutlu <mutlu.erdem@soft-nec.com>
2018-08-01 00:14:55 +0300

    first commit

---
+++ src/main/java/com/softnec/liftnec/Application.java
@@ -0,0 +1,21 @@
+package com.softnec.liftnec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+	@RequestMapping("/")
+	public String home() {
+		return "SoftNec's first running micro-service docker container";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/softnec/liftnec/model/remote/UserDto.java
@@ -0,0 +1,43 @@
+public class UserDto {
+    String id;
+    String firstName;
+    String lastName;
+    String userName;
+    String profile;
+
+    public UserDto() {
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
